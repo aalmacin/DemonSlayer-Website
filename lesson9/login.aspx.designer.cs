91501c4a40b7a666b0d00793266c9e023dbc0969
mike yarnold <mfjy@outlook.com>
2013-08-06 01:54:56 -0400

    got pages working with roles

--- lesson9/login.aspx.designer.cs
+++ lesson9/login.aspx.designer.cs
@@ -47,5 +47,14 @@ namespace lesson9 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogin;
+        
+        /// <summary>
+        /// RegisterLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink RegisterLink;
     }
 }
